6b03a052e3738501d3010e5961cdf95290853782
biancadmsadf16@gmail.com <biancadmsadf16@gmail.com>
2024-03-20 21:01:05 -0300

    mi segundo commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
-
 const ingresarNumero = parseInt(prompt("Ingresa un número del 1 al 20 para realizar operaciones aritméticas"));
 
 const multiplicar = (numero) => {
     let resultado = "";
-    let factorial = 1; // Parte el factorial en 1
     if (numero >= 1 && numero <= 20) {
         // Calculo de  multiplicaciones
-        for (let i = 1; i <= 20; i++) {
+        for (let i = 1; i <= numero; i++) {
             resultado += i + " x " + numero + " = " + (i * numero) + "<hr>" + "<br>";
         }
 
-        // Calculo el factorial 
-        for (let j = 1; j <= numero; j++) {
-            factorial *= j;
+        // Cálculo del factorial
+        for (let i = 1; i <= numero; i++) {
+            let factorial = 1; // Se inicializa el factorial dentro del bucle en 1
+            for (let j = 1; j <= i; j++) {
+                factorial *= j;
+            }
+            resultado +=  "El Factorial del " + i + " es: " + factorial + "<hr>" + "<br>"; 
         }
-        
-        // Mostrar el factorial
-        resultado +=  "El Factorial del " + numero + " es " + factorial + "<hr>" + "<br>";
     } else {
         resultado = "Número fuera de rango";
     }
